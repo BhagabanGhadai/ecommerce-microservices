384ed061dd53da891b26caa89c8e9fae5341d6d7
Bhagaban Ghadai <102967585+BhagabanGhadai@users.noreply.github.com>
2023-10-10 00:41:26 +0530

    blacklisted token added auth middleware implemented

--- userService/src/server.js
+++ userService/src/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import ApiError from './util/apiError.js';
 import { errorHandler } from './util/middleWare/error-handler.js';
-
+import { verifyBlackList } from './util/middleWare/auth.middleware.js';
+import env from '../src/env.js'
 import healthCheckRouter from './app/healthCheck/healthCheck.routes.js'
+
+
 export const expressApp = async (app) => {
+   app.use(verifyBlackList)
    app.use(express.json())
    app.use(express.urlencoded({ extended: true }))
+   app.use(cookieParser());
+   if (env.NODE_ENV == "dev") app.use(morgan('dev'))
    app.use('/api/v1/healthcheck',healthCheckRouter)
    app.use((req,res,next)=>{
       throw new ApiError(404,"Api not found")
