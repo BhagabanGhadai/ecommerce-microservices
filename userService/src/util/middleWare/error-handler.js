d0b85f0eaa60f534d26177217304029cbd139dfe
Bhagaban Ghadai <102967585+BhagabanGhadai@users.noreply.github.com>
2023-10-10 16:34:24 +0530

    winston logger is added to log the error

--- userService/src/util/middleWare/error-handler.js
+++ userService/src/util/middleWare/error-handler.js
@@ -3,6 +3,19 @@ import env from '../../env.js'
 import ApiError  from '../apiError.js';
 import winston from 'winston'
 
+// Configure Winston to log errors to a file
+const errorLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' })
+  ]
+});
+
 export const errorHandler = (err, req, res, next) => {
   let error = err;
   if (!(error instanceof ApiError)) {
@@ -15,6 +28,6 @@ export const errorHandler = (err, req, res, next) => {
     message: error.message,
     ...(env.NODE_ENV === "development" ? { stack: error.stack } : {}),
   };
-
+  errorLogger.error(response)
   return res.status(error.statusCode).send(response);
 };
